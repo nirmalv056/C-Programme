1b25958c30e717e9f37c511b7f5a0011da286b10
nirmalv056 <128505766+nirmalv056@users.noreply.github.com>
2023-08-25 13:34:45 +0530

    Add files via upload

---
+++ Addition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	int num1,num2;
+	printf("Enter the num1:");
+	scanf("%d",&num1);
+	
+	printf("Enter the num2:");
+	scanf("%d",&num2);
+	
+	printf("The addition is:%d",num1+num2);
+	
+	getch();
+}
---
+++ Arithmetic_Operator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	int a,b;
+	a=35;
+	b=6;
+	
+	printf("a+b=%d \n",a+b);
+	printf("a-b=%d \n",a-b);
+	printf("a*b=%d \n",a*b);
+	printf("a/b=%d \n",a/b);
+	printf("a%b=%d \n",a%b);
+	
+	getch();
+}
