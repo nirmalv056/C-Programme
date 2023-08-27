fa2f7d8ce1dfbd1b758ec35e09cfedbda30d18e9
Nirmal Kumar Verma <128505766+nirmalv056@users.noreply.github.com>
2023-08-27 12:02:12 +0530

    Add files via upload

---
+++ Logical_Operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	int a,b;
+	a=5;
+	b=6;
+	
+	printf("a and b=%d \n",a && b);
+	printf("a or b=%d \n",a || b);
+	printf("a*b=%d \n",!a && b);
+	
+	getch();
+}
