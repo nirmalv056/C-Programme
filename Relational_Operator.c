2557731f04ccf1cc8ef602c192fcf0590a9d272c
Nirmal Kumar Verma <128505766+nirmalv056@users.noreply.github.com>
2023-08-27 12:04:04 +0530

    Add files via upload

---
+++ Relational_Operator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	int a,b;
+	a=5;
+	b=6;
+	
+	printf("a is equal to b=%d \n",a==b);
+	printf("a is not equal to b=%d \n",a!=b);
+	printf("a greater than b=%d \n",a>b);
+	printf("a less than b=%d \n",a<b);
+	printf("a is greater than or equal to b=%d \n",a>=b);
+	printf("a is less than or equal to b=%d \n",a>=b);
+
+	getch();
+}
